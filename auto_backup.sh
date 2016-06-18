e8aea84e373493542bb7d48a9a741c9b4702f7b6
zeroway <M_O_Bz@163.com>
2016-06-18 21:14:34 +0000

    sync form my local pc

--- auto_backup.sh
+++ auto_backup.sh
@@ -7,10 +7,11 @@ MAKE_CONFIG="/etc/portage/make.conf"
 PACKAGE_USE="/etc/portage/package.use/use"
 PACKAGE_MASK="/etc/portage/package.mask/mask"
 PACKAGE_UNMASK="/etc/portage/package.unmask"
-PACKAGE_ACCEPT_KEYWORDS="/etc/portage/package.accept_keywords"
+PACKAGE_ACCEPT_KEYWORDS="/etc/portage/package.accept_keywords/accept_keywords"
 REPOS_GENTOO="/etc/portage/repos.conf/gentoo"
 REPOS_LOCAL="/etc/portage/repos.conf/local.conf"
 RSYNC_EXCLUDES="/etc/portage/rsync_excludes"
+PACKAGE_LICENSE="/etc/portage/package.license/license"
 
 # 把需要备份的文件放在这个数组中
 copy_files_name=(
@@ -28,7 +29,7 @@ $RSYNC_EXCLUDES
 # 遍历数组里的文件进行复制
 for file in ${copy_files_name[@]}
 do
-	echo $file 'exist, ready to backup...'
+	# echo $file 'exist, ready to backup...'
 	# file exists and is a regular file
 	if [ -f $file ]; then
 		# 先创建一个空的对应目录的文件
@@ -39,6 +40,8 @@ do
 		# 备份
 		cp $file ${file#*/}
 
-		echo "backup to" ${file#*/} "done!!"
+	#	echo "backup to" ${file#*/} "done!!"
 	fi
 done
+
+echo "All things have get done."
