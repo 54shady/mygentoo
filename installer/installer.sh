2f3284db7f9de9c1177887adcc04e5a7a4e4d601
zeroway <M_O_Bz@163.com>
2021-09-08 10:34:52 +0800

    Installer : add fstab for auto deploy script

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,44 +1,39 @@
 #!/bin/bash
 
 UTIL_PATH=$PWD
-
-echo "Start install gentoo;-)"
-read -p "Yes/No " TMP
+CHROOT="/mnt/gentoo"
+HOST_NAME="Gentoo"
 
 grep "/mnt/gentoo" /proc/mounts
-if [ $? -eq 0 ]
-then
-	umount /mnt/gentoo
-	rm -rf /mnt/gentoo
-	mkdir /mnt/gentoo
-fi
+[ $? -eq 0 ] && { umount $CHROOT; rm -rf $CHROOT; mkdir $CHROOT; }
 
-echo "Input Target Disk"
-read devx
-echo $devx
-if [ $TMP == y ]; then
-	echo "Start Installation process"
-	$UTIL_PATH/autopartx.sh $devx
-else
-	echo "No"
-fi
+read -p "Install gentoo;-) [Y]es/[N]o: " TMP
+[ $TMP == y ] && \
+	{ read -p "Target Disk " devx; $UTIL_PATH/autopartx.sh $devx; } || \
+	{ echo "Abort installation"; exit; }
 
-mount ${devx}2 /mnt/gentoo
-mkdir /mnt/gentoo/boot
-mount ${devx}1 /mnt/gentoo/boot
+mount ${devx}2 $CHROOT
+mkdir $CHROOT/boot
+mount ${devx}1 $CHROOT/boot
 
-echo "Input stage3 tar ball"
-read stage
+read -p "Input Stage3 tarball: " stage
+tar jxvf $stage -C $CHROOT/
 
-tar jxvf $stage -C /mnt/gentoo/
+mount -t proc /proc $CHROOT/proc
+mount --rbind /sys $CHROOT/sys
+mount --make-rslave $CHROOT/sys
+mount --rbind /dev $CHROOT/dev
+mount --make-rslave $CHROOT/dev
 
-mount -t proc /proc /mnt/gentoo/proc
-mount --rbind /sys /mnt/gentoo/sys
-mount --make-rslave /mnt/gentoo/sys
-mount --rbind /dev /mnt/gentoo/dev
-mount --make-rslave /mnt/gentoo/dev
+# update fstab
+cp fstab $CHROOT/etc/
+diskname=${devx##*/}
+sed -i -e "s/bootp/${diskname}1/" \
+	-e "s/rootp/${diskname}2/" \
+	-e "s/homep/${diskname}4/" \
+	-e "s/swapp/${diskname}3/" \
+	$CHROOT/etc/fstab
 
-cp deploy.sh /mnt/gentoo/root/deploy.sh
-chmod +x /mnt/gentoo/root/deploy.sh
-HOST_NAME="newdeploy"
-chroot /mnt/gentoo /root/deploy.sh $HOST_NAME
+cp deploy.sh $CHROOT/root/deploy.sh
+chmod +x $CHROOT/root/deploy.sh
+chroot $CHROOT /root/deploy.sh $HOST_NAME
